34b3751d8183a1463408cb093256de44ea9b4919
Smrutiranjan Patra <76831153+Smrutiranjan-Patra@users.noreply.github.com>
2022-01-18 13:57:39 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import React from 'react'
+import Header from './components/header'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+      <Header />
     </div>
   );
 }
