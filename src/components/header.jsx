5f61ce60709d73046fea9a9685cd9a8749957b16
Smrutiranjan Patra <76831153+Smrutiranjan-Patra@users.noreply.github.com>
2022-01-19 13:32:43 +0530

    update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import "./css/header.css";
+import mainlogo from "./Images/mainlogo.png";
 
 export default function header() {
-  return <h1>This is Header</h1>;
+  return (
+    <>
+      <div className="header">
+        <div className="centered">
+          <div className="logo-box">
+            <a href="#">
+              <img src={mainlogo} alt="" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
