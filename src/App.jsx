cbb0323b31f897f388db64e829d8ca0de145fc80
Madhurima Banerjee <91532700+MADHURIMA956@users.noreply.github.com>
2022-01-19 12:44:23 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Register } from "./components/Register";
 
 function App() {
   return (
     <div className="App">
-     
+     <Register />
     </div>
   );
 }
