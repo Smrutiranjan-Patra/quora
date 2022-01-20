9374315365d69f7ea4abb7a12161694a413d34bb
Smrutiranjan Patra <76831153+Smrutiranjan-Patra@users.noreply.github.com>
2022-01-21 01:18:03 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="app">
       {/* <Header /> */}
-      <Router />
+      <Header />
     </div>
   );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./css/header.css";
 import mainlogo from "./Images/mainlogo.png";
+import { NavLink } from "react-router-dom";
 
 //icons
-import HomeIcon from "@mui/icons-material/Home";
-import NotificationsIcon from "@mui/icons-material/Notifications";
-import PersonIcon from "@mui/icons-material/Person";
-import CreateIcon from "@mui/icons-material/Create";
-import FeedIcon from "@mui/icons-material/Feed";
-import LanguageIcon from "@mui/icons-material/Language";
+// import HomeIcon from "@mui/icons-material/Home";
+// import NotificationsIcon from "@mui/icons-material/Notifications";
+// import PersonIcon from "@mui/icons-material/Person";
+// import CreateIcon from "@mui/icons-material/Create";
+// import FeedIcon from "@mui/icons-material/Feed";
+// import LanguageIcon from "@mui/icons-material/Language";
 
 
 
@@ -28,6 +29,13 @@ export default function Header() {
         <div className="centered">
           <div className="logo-box">
             <img src={mainlogo} alt="" />
+          </div>
+          <div className="Navbar">
+            <NavLink exact activeClassName="active-icon" to="/Home"></NavLink>
+            <NavLink exact activeClassName="active-icon" to="/following"></NavLink>
+            <NavLink exact activeClassName="active-icon" to="/answer"></NavLink>
+            <NavLink exact activeClassName="active-icon" to="/space"></NavLink>
+            <NavLink exact activeClassName="active-icon" to="/notification"></NavLink>
           </div>
         </div>
       </div>
