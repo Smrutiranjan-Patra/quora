53fa55a441037d83e4edcb6a4ab77cd17d6ed374
abhi270794 <86410022+abhi270794@users.noreply.github.com>
2022-01-21 21:19:01 +0530

    Profile added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import Header from "./components/header";
 import "./App.css";
+// import {Profile} from './Pages/Profile'
+import { Router } from "./Routes/Router";
+
 // import { Register } from "./components/Register";
 
 function App() {
   return (
     <div className="app">
+
+
+      {/* <Header />
+       */}
+      <Router />
+      
+
     </div>
 
   );
